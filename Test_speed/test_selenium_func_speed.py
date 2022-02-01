8c88e02cb63c0f10572ed6bdf96d2caace57fcc5
Ayoub <68515047+ni2mi@users.noreply.github.com>
2022-02-01 13:51:48 +0100

    selenium maj

--- Test_speed/test_selenium_func_speed.py
+++ Test_speed/test_selenium_func_speed.py
@@ -13,23 +13,24 @@ class Selenium_test:
         driver.find_element_by_name('submit').click()
         
         # Assert h1 field is correct in this page
-        h1 = driver.find_element_by_tag_name('h1')
-        assert h1.text == "CANDIDATURES"
+        # Try + assert ou l'un ou l'autre uniquement ? 
+        try:
+            h1 = driver.find_element_by_tag_name('h1')
+            assert h1.text == "CANDIDATURES"
+        except AttributeError:
+            print('ERROR ----- h1 not found')
         # Assert Flash login success 
         flash_login = driver.find_element_by_class_name('alert')
         assert 'Vous êtes connecté en tant que :' in flash_login.text 
         print('------------------test login user Done----------------------')
 
     def add_candidacy_test(test_name, contact_test):
-        # Click the button add candidacy
-        # Need id button
-        add_xpath = '//button[@class="btn btn-lg btn-primary mt-4 d-block"]/a'
-        driver.find_element(By.XPATH, add_xpath).click()
+        # To click to the button add candidacy
+        driver.find_element_by_link_text('Ajouter candidature').click()
+        
         # Insert the candidacy in the form 
         driver.find_element_by_id('name').send_keys(test_name) 
-    
         driver.find_element_by_id('place').send_keys('Lille') 
-
         driver.find_element_by_id('contact').send_keys(contact_test) 
 
         # Click on the button add candidacy
@@ -50,7 +51,7 @@ class Selenium_test:
         # Click on the third button delete item 
         delete_xpath = '//div[@class="tbl-content"]/table/tbody/tr[3]/td[7]/a[2]'
         driver.find_element(By.XPATH, delete_xpath).click()
-        # assert 'word' not in [xpath row[1]] need order dashboard by last added
+        # assert 'word' not in [xpath row[1]] need order dashboard by last added 
         # Or need to use Candidacy.query.filter_by(name=[element to check]) methods ?
         print('------------------delete candidacy n°3 done----------------------')
         
@@ -58,9 +59,7 @@ class Selenium_test:
 
     def logout_test():
         # Click on button logout 
-        logout_xpath = '//body/nav/div[@class="col-3 text-right"]/span[2]'
-        # driver.find_element_by_class_name('nav-link').click() [Need unique ID. Else xpath]
-        driver.find_element(By.XPATH, logout_xpath).click()
+        driver.find_element_by_link_text('Log out').click()
         #Assert flash sucess loging 
         flash_logout = driver.find_element_by_class_name('alert')
         assert 'Vous êtes correctement déconnecté' in flash_logout.text
@@ -68,9 +67,7 @@ class Selenium_test:
 
 
     def click_login():
-        # Need id 
-        click_login_xpath = '//span/a[@class="nav-link"]'
-        driver.find_element(By.XPATH, click_login_xpath).click()
+        driver.find_element_by_link_text('Log in').click()
         print('---------------Click to login---------------------------')
 
     def modify_candidacy():
